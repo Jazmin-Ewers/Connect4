c1bc510ad601d2c78d3c8b30407c89d58d5c5881
jaz_ewe <jazminewers11@gmail.com>
2021-11-19 18:16:42 -0500

    add state variables

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
-board = [[null, null, null,  null, null, null, null] 
+/*----- constants -----*/
+/*----- app's state (variables) -----*/
+let turn, winner; 
+let board = [[null, null, null,  null, null, null, null] 
          [null, null, null, null, null, null, null],
          [null, null, null, null, null, null, null],
          [null, null, null, null, null, null, null],
          [null, null, null, null, null, null, null],
          [null, null, null, null, null, null, null],
         ];
+/*----- cached element references -----*/
+/*----- event listeners -----*/
+/*----- functions -----*/
+
