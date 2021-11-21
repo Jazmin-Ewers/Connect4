d6b447772229d4888e0878dc40bf2fc4175c9ae5
jaz_ewe <jazminewers11@gmail.com>
2021-11-21 00:20:32 -0500

    figuring out the stackable aspect of Connect4

--- main.js
+++ main.js
@@ -9,7 +9,6 @@ const lookup = {
 
 /*----- cached element references -----*/
 const circleEls = document.querySelectorAll('span') // select all the circles in the form of a Nodelist
-console.log(circleEls);
 /*----- event listeners -----*/
 document.querySelector('table').addEventListener('click', handleMove);
 /*----- functions -----*/
@@ -17,11 +16,11 @@ init(); // sets up state variables
 
 function init(){
     board = [[null, null, null,  null, null, null, null], 
-             [null, null, null, null, null, null, null],
-             [null, null, null, null, null, null, null],
-             [null, null, null, null, null, null, null],
-             [null, null, null, null, null, null, null],
-             [null, null, null, null, null, null, null]
+             [null, 1, null, null, null, null, null],
+             [null, 1, null, null, null, null, null],
+             [null, 1, null, null, null, null, null],
+             [null, 1, null, null, 1, null, 1],
+             [null, 1, null, null, 1, null, 1]
              ];
     turn = 1;
     winner = null; 
@@ -44,8 +43,22 @@ function handleMove(evt){
     //Grab index value from the square we click on
     let rowNumberIndex = parseInt(evt.target.id.replace('cir', '')[0]);
     let elementIndex = parseInt(evt.target.id.replace('cir', '')[1]);
-    // Check if square clicked on is available
+    // Check if circle clicked is available
     if (board[rowNumberIndex][elementIndex]) return; // both 1 and -1 are truthy and null is falsey
+    // Check if circle click has a full column beneath it 
+    // Loop through X times depending on how many spaces are underneath the circle clicked 
+    let row = rowNumberIndex;
+    for (let i = 0; i < (6 - rowNumberIndex); i++){
+        // As soon as there is a null "empty space" beneath the circle click return/exit the function
+        if (board[row][elementIndex] === null) {
+            console.log(`null ${row}, ${elementIndex}`);
+            row ++;
+        } else {
+            console.log(`Not null ${row}, ${elementIndex}`)
+        }
+    }
+    //console.log([rowNumberIndex + 1], [elementIndex])
+    // && board[rowNumberIndex + 1][elementIndex] === null
     board[rowNumberIndex][elementIndex] = turn;
     turn *= -1;
     render();
