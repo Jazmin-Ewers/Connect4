82acdb95b86fe9c53bc73d991633192aeb8575e5
jaz_ewe <jazminewers11@gmail.com>
2021-11-22 22:16:25 -0500

    add start game button

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-/*----- constants -----*/
 /*----- app's state (variables) -----*/
 let turn, winner, board;
 const lookup = {
@@ -6,101 +5,137 @@ const lookup = {
     "-1": "black", 
     null: "white"
 };
-
 /*----- cached element references -----*/
-const circleEls = document.querySelectorAll('span') // select all the circles in the form of a Nodelist
-/*----- event listeners -----*/
-document.querySelector('table').addEventListener('click', handleMove);
-const message = document.querySelector("h1");
-/*----- functions -----*/
-init(); // sets up state variables
+let message = document.querySelector("h1");
 
+/*----- event listeners -----*/
+document.getElementById('startbutton').addEventListener('click', startGame);
+document.getElementById('resetbutton').addEventListener('click', init);
+
+/*----- functions -----*/
 function init(){
     board = [[null, null, null,  null, null, null, null], 
-             [-1, null, null, null, null, null, null],
-             [-1, -1, null, null, null, null, null],
-             [-1, -1, -1, null, null, null, null],
-             [-1, -1, -1, 1, null, null, null],
-             [-1, -1, -1, -1, null, null, null]
-             ];
+            [null, null, null, null, null, null, null],
+            [null, null, null, null, null, null, null],
+            [null, null, null, null, null, null, null],
+            [null, null, null,null, null, null, null],
+            [null, null, null, null, null, null, null]
+            ];
     turn = 1;
     winner = null; 
     render();
 }
-
-
+function startGame(){
+    init();
+    document.querySelector('table').addEventListener('click', handleMove);
+}
+ 
 function render(){
-    // Loop through row and then through each element
-    // If board[][] null : change background to white, 1 : change background to red,  -1: change background to black 
-    // According to board[index] value change the background image to object's lookup[sq] value 
+    let count = 0;
+    // Loop through each row and then through each element.
+    // For each board space set the background to white, red or black.  
+    // If board space is 1 or -1 add 1 to the count. (This will be used to determined the tie). 
     board.forEach(function(row, rowIdx){
         row.forEach(function(element, elementIdx){
             let currentBoardCircle = board[rowIdx][elementIdx];
             let domBoardCircle = document.getElementById(`cir${rowIdx}${elementIdx}`);
             domBoardCircle.style.backgroundColor = lookup[currentBoardCircle];
+            if (board[rowIdx][elementIdx] === 1 || board[rowIdx][elementIdx] === -1 ){
+                count++;
+            }
         });
     });
+    //Check if the board is full with -1's and 1's.
+    if (count === 42){
+        winner = "T";
+    }
+    // Check who the winner is and change the text on the HTML page to display message.
     if (winner === "T") {
-        message.innerHTML = "It's a tie!";
-      } else if (winner) {
-        message.innerHTML = `Player ${lookup[winner]} won!`;
-      } else {
-        message.innerHTML = `It's Player ${lookup[turn]}'s Turn`;
-      }
-    }  
+        return message.innerHTML = "It's a tie!";
+    } else if (winner === 1 || winner === -1 ) {
+        return message.innerHTML = `Player ${lookup[winner]} won!`;
+    } else {
+        return message.innerHTML = `It's Player ${lookup[turn]}'s Turn`;
+    }
+} 
+
 function getWinner(){
-    //Get Column winner
-//  for (let i = 0; i < 7; i++) {
-//      if (Math.abs(board[0][i] +  board[1][i] + board[2][i] + board[3][i]) === 4) {
-//          console.log('Winner by Column [0-3] index');
-//      } else if (Math.abs(board[1][i] +  board[2][i] + board[3][i] + board[4][i]) === 4){
-//          console.log('Winner by Column [1-4] index');
-//      } else if (Math.abs(board[2][i] +  board[3][i] + board[4][i] + board[5][i]) === 4){
-//          console.log('Winner by Column [2-5] index');
-//      }
-//  }
-//  //Get Row winner
-//  for (let i = 0; i < 6; i++) { 
-//      if (Math.abs(board[i][0] + board[i][1] + board[i][2] + board[i][3]) === 4) {
-//          console.log('Winner by Row [0-3] index')
-//      } else if (Math.abs(board[i][1] +  board[i][2] + board[i][3] + board[i][4]) === 4){
-//          console.log('Winner by Row [1-4] index')
-//      } else if (Math.abs(board[i][2] + board[i][3] + board[i][4] + board[i][5]) === 4){
-//          console.log('Winner by Row [2-5] index')
-//      } else if (Math.abs(board[i][3] +  board[i][4] + board[i][5] + board[i][6]) === 4){
-//          console.log('Winner by Row [3-6] index')
-//      }
-//  }
- //Get Diagonal winner
-let index1DiagonalLoop = 0;
-let index2DiagonalLoop = 1;
-let index3DiagonalLoop = 2;
-let index4DiagonalLoop = 3;
+// Get Column winner
+    for (let i = 0; i < 7; i++) {
+        if (Math.abs(board[0][i] +  board[1][i] + board[2][i] + board[3][i]) === 4) {
+            return board[0][i];
 
-    for (let i = 0; i < 3; i++) {
-        if (Math.abs(board [0][index1DiagonalLoop] +  board[1][index2DiagonalLoop] + board[2][index3DiagonalLoop] + board[3][index4DiagonalLoop]) === 4){
-            console.log(`Winner by Diagonal index 0${index1DiagonalLoop} , 1${index2DiagonalLoop}, 2${index3DiagonalLoop}, 3${index4DiagonalLoop}`)
-        } else {
-            index1DiagonalLoop ++;
-            index2DiagonalLoop ++;
-            index3DiagonalLoop ++;
-            index4DiagonalLoop ++;
+        } else if (Math.abs(board[1][i] +  board[2][i] + board[3][i] + board[4][i]) === 4){
+            return board[1][i];
+
+        } else if (Math.abs(board[2][i] +  board[3][i] + board[4][i] + board[5][i]) === 4){
+            return board[2][i];
         }
+    }
+// Get Row winner
+    for (let i = 0; i < 6; i++) { 
+        if (Math.abs(board[i][0] + board[i][1] + board[i][2] + board[i][3]) === 4) {
+            return board[i][0];
+
+        } else if (Math.abs(board[i][1] +  board[i][2] + board[i][3] + board[i][4]) === 4){
+            return board[i][1];
+
+        } else if (Math.abs(board[i][2] + board[i][3] + board[i][4] + board[i][5]) === 4){
+            return board[i][2];
+
+        } else if (Math.abs(board[i][3] +  board[i][4] + board[i][5] + board[i][6]) === 4){
+            return board[i][3];
+        }
+    }
+
+// Get Diagonal winner
+    for (let i = 0; i < 4; i++) {
+        let diagonalIndex1 = 0;
+        let diagonalIndex2 = 1;
+        let diagonalIndex3 = 2;
+        let diagonalIndex4 = 3;
+        if (Math.abs(board[0][diagonalIndex1] +  board[1][diagonalIndex2] + board[2][diagonalIndex3] + board[3][diagonalIndex4]) === 4){
+            console.log(`Won 0${diagonalIndex1} + 1${diagonalIndex2} + 2${diagonalIndex3} + 3${diagonalIndex4}`)
+            return board[0][diagonalIndex1];
+        } else if (Math.abs(board[1][diagonalIndex1] +  board[2][diagonalIndex2] + board[3][diagonalIndex3] + board[4][diagonalIndex4]) === 4){
+            return board[1][diagonalIndex1];
+
+        } else if (Math.abs(board[2][diagonalIndex1] +  board[3][diagonalIndex2] + board[4][diagonalIndex3] + board[5][diagonalIndex4]) === 4){
+            return board[2][diagonalIndex1];
+                
+        } else if (Math.abs(board[3][diagonalIndex1] +  board[2][diagonalIndex2] + board[1][diagonalIndex3] + board[0][diagonalIndex4]) === 4){
+            return board[3][diagonalIndex1];
+                
+        } else if (Math.abs(board[4][diagonalIndex1] +  board[3][diagonalIndex2] + board[2][diagonalIndex3] + board[1][diagonalIndex4]) === 4){
+            return board[4][diagonalIndex1];
+                
+        } else if (Math.abs(board[5][diagonalIndex1] +  board[4][diagonalIndex2] + board[3][diagonalIndex3] + board[2][diagonalIndex4]) === 4){
+            return board[2][diagonalIndex1];
+                
+        } else {
+            diagonalIndex1 ++;
+            diagonalIndex2 ++;
+            diagonalIndex3 ++;
+            diagonalIndex4 ++;
+        }
+    }
 }
-}
-
-
-
+    
 function handleMove(evt){
-    //Grab index value from the square we click on
+    if (winner === 1 || winner === -1){
+        return
+    }
+    // Grab index value from the circle we click on
     let rowNumberIndex = parseInt(evt.target.id.replace('cir', '')[0]);
     let elementIndex = parseInt(evt.target.id.replace('cir', '')[1]);
+
     // Check if circle clicked is available
-    if (board[rowNumberIndex][elementIndex]) return; // both 1 and -1 are truthy and null is falsey
-    // Check if circle click has a full column beneath it 
-    // Loop through X times depending on how many spaces are underneath the circle clicked 
+    if (board[rowNumberIndex][elementIndex]) return;
+
+    // Check if circle click has a full column beneath it
     let row = rowNumberIndex;
     for (let i = 0; i < (6 - rowNumberIndex); i++){
+        // Looping through X times depending on how many spaces are underneath the circle clicked. 
         // As soon as there is a null "empty space" beneath the circle click return/exit the function
         if (row > rowNumberIndex && board[row][elementIndex] === null) {
             return;
@@ -112,4 +147,10 @@ function handleMove(evt){
     turn *= -1;
     winner = getWinner();
     render();
-    }
+}
+
+
+
+
+
+
